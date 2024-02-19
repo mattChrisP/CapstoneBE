82ab30e2ec4aaa9d82e1548f812e62493db91228
mattChrisP <matthewchristopherpohadi@gmail.com>
2024-02-19 13:51:30 +0000

    feat: update cam fetch

--- camera.py
+++ camera.py
@@ -10,8 +10,8 @@ def get_image(camera_device):
     if img_idx == BUFFER+1:
         img_idx = 1
 
-    os.system(f"libcamera-still -o obj{img_idx}CAM{camera_device}.jpg --width 640 --height 640 --rotation 180 --timeout 1 --nopreview --device {camera_device}")
-    
+    os.system(f"fswebcam -d /dev/video0 -r 1280x720 --no-banner test.jpg")
+
 
 # Example usage
 # camera_device = "/dev/video0"  # Change this based on your camera (use /dev/video1 for the other camera)
