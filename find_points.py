8441f51f7ac034736005487e0cf976511f791451
mattChrisP <matthewchristopherpohadi@gmail.com>
2024-02-20 13:34:18 +0000

    temp

--- find_points.py
+++ find_points.py
@@ -17,7 +17,7 @@ def select_point(event, x, y, flags, param):
             complete = True
 
 # Load the image
-image = cv2.imread('obj1.jpg')
+image = cv2.imread('obj1cam1.jpg')
 if image is None:
     print("Failed to load image")
     exit()
